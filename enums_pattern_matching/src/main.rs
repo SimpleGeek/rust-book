997e59f5d45b43fd283d4cda304c747814854c15
SimpleGeek <isaiah@livebythecode.us>
2021-04-24 12:50:26 -0400

    Did work on enums

--- enums_pattern_matching/src/main.rs
+++ enums_pattern_matching/src/main.rs
@@ -29,8 +29,51 @@ fn main() {
     };
 
     let new_home = IpAddr::V4(127, 0, 0, 1);
+
+    // We can use the Option enum to account for missing values
+    let not_here: Option<u8> = None;
+    println!("Is not here, well, here? {}", not_here.is_some());
+
+    println!("Value in cents of coin type {:?} is {}", Coin::Nickel, value_in_cents(Coin::Nickel));
 }
 
 fn route(ip_kind: IpAddrKind) {
     println!("Routing IP address of type {:?}", ip_kind);
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Arkansas,
+    Alaska,
+    Hawaii,
+    North_Carolina,
+    South_Carolina,
+    West_Virginia,
+    Vermont,
+    Virginia,
+    // etc
+}
+
+#[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
 }
